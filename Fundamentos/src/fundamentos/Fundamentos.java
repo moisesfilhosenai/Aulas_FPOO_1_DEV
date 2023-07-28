640689679aa08a214d7c68ef4b451f4507c9a7cd
Moises <moisesolimpio99@gmail.com>
2023-07-28 11:07:40 -0300

    Entendendo Wrappers

--- Fundamentos/src/fundamentos/Fundamentos.java
+++ Fundamentos/src/fundamentos/Fundamentos.java
@@ -15,6 +15,11 @@ public class Fundamentos {
         double media = 5.5; 
         
         String bairro = new String("Centro");
+        
+        // Declaração de Wrappers
+        int nota1 = Integer.parseInt("8");
+        double mediaF = Double.parseDouble("5.5");
+        boolean aprovado = Boolean.parseBoolean("true");
     }
     
 }
