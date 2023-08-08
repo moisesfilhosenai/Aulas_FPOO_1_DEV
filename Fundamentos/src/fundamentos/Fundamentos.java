683b4f7020ede8441722394afb9b08e15dcc0294
Moises Olimpio <moisesolimpio@Moisess-MacBook-Air.local>
2023-08-08 15:44:28 -0300

    Atualizando repositorio

--- Fundamentos/src/fundamentos/Fundamentos.java
+++ Fundamentos/src/fundamentos/Fundamentos.java
@@ -20,6 +20,10 @@ public class Fundamentos {
         int nota1 = Integer.parseInt("8");
         double mediaF = Double.parseDouble("5.5");
         boolean aprovado = Boolean.parseBoolean("true");
+
+        System.out.println("Aluno: " + nome + " Idade: " + idade + " Estudando: "+ estudando);
+        System.out.println("Média: " + media + " Bairro: " + bairro + " Nota 1: " + nota1 + " Media: " + media);
+        System.out.println("Média Final: " + mediaF +  "Aprovado: " + aprovado);
     }
     
 }
