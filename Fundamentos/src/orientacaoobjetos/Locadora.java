bd45d7169e7cde437e958cfc42b96dc355695525
Moises Olimpio <moises.filho@docente.senai.br>
2023-08-31 10:21:27 -0300

    Criando interfaces para validar nome e email do cliente

--- Fundamentos/src/orientacaoobjetos/Locadora.java
+++ Fundamentos/src/orientacaoobjetos/Locadora.java
@@ -12,11 +12,16 @@ public class Locadora {
         double valorLocacaoSimples = c1.calcularLocacao(3);
         System.out.println("O valor para locação do filme "
                 + "simples é: " + valorLocacaoSimples);
+        c1.validarNome(c1.getNome());
+        c1.verificarEmailDominioGoogle(c1.getEmail());
+        
         
         Cliente c2 = new Cliente("Marilza Sílva", "marilza@gmail.com");
         System.out.println(c2);
         double valorLocacaoLancamento = c2.calcularLocacao(3, true);
         System.out.println("O valor para locação do filme "
                 + "lançamento é: " + valorLocacaoLancamento);
+        c2.validarNome(c2.getNome());
+        c2.verificarEmailDominioGoogle(c2.getEmail());
     }
 }
